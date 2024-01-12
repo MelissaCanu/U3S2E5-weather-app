a1ed3cc2bddf031c6d6a81ad1b98cb3c3c42043e
MelissaCanu <melissamcanu@gmail.com>
2024-01-12 16:49:17 +0100

    almost there

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -12,7 +12,7 @@ const Forecast = () => {
 			try {
 				const apiKey = "14bce469fb02143aaed162ec1858afea";
 				const response = await fetch(
-					`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=1&appid=${apiKey}`
+					`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&appid=${apiKey}`
 				);
 
 				if (!response.ok) {
@@ -30,6 +30,23 @@ const Forecast = () => {
 		getForecastData();
 	}, [city]);
 
+	/*  prendi il giorno della settimana - se ti ricordi come fare */
+
+	const getDayOfWeek = (dateString) => {
+		const weekDays = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+		];
+		const date = new Date(dateString);
+		const dayIndex = date.getDay();
+		return weekDays[dayIndex];
+	};
+
 	return (
 		<div>
 			{forecastData ? (
@@ -38,18 +55,27 @@ const Forecast = () => {
 					<Row xs={1} md={2} lg={4} className="g-4">
 						{forecastData.list.map((item, index) => (
 							<Col key={index}>
-								<Card>
-									<Card.Body>
-										<Card.Title>{item.weather[0].main}</Card.Title>
+								<Card
+									style={{
+										backgroundColor: "#87CEEB",
+										border: "none",
+										boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+									}}
+								>
+									<Card.Body className="card-background">
+										<Card.Title>{getDayOfWeek(item.dt_txt)}</Card.Title>
+										<Card.Text>
+											<strong>Temperature:</strong> {item.main.temp} °C
+										</Card.Text>
+										<Card.Text>
+											<strong>Weather:</strong> {item.weather[0].main}
+										</Card.Text>
 										<Card.Text>
 											<strong>Description:</strong>{" "}
 											{item.weather[0].description}
-										</Card.Text>
-										<Card.Text>
-											<strong>Icon:</strong> {item.weather[0].icon}
-										</Card.Text>
-										<Card.Text>
-											<strong>Temperature:</strong> {item.main.temp} °C
 										</Card.Text>
 										<Card.Text>
 											<strong>Humidity:</strong> {item.main.humidity}%
