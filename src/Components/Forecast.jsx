8e147f866bcdeb9f10b26d959acb0fe06ec68bfb
MelissaCanu <melissamcanu@gmail.com>
2024-01-12 17:27:02 +0100

    styling cards, replicating function

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -47,6 +47,21 @@ const Forecast = () => {
 		return weekDays[dayIndex];
 	};
 
+	/* bg image */
+
+	const getBackgroundImage = (weather) => {
+		const backgroundImages = {
+			Clear:
+				"url(https://media2.giphy.com/media/RqSJ6nQVsOpxe/200.webp?cid=ecf05e47b5sdwo0oze5kp98289o9t4m825cwqy4gnvmp32he&ep=v1_gifs_search&rid=200.webp&ct=g)",
+			Clouds:
+				"url(https://media3.giphy.com/media/VmoYjGrAccGCo50ahx/giphy.gif?cid=ecf05e47x0nvhz4gsdccx534xu4ec4t0d5567d2u1gufvx9o&ep=v1_gifs_search&rid=giphy.gif&ct=g)",
+			Rain: "url(https://media0.giphy.com/media/39fj7g99qyD72/giphy.gif?cid=ecf05e47goepd7p6srvqf1qw5b9ldx8n943ion2n01rnrn1i&ep=v1_gifs_search&rid=giphy.gif&ct=g)",
+			Snow: "url(https://media1.giphy.com/media/ZdlSqZuzzMB1JT3nwT/giphy.gif?cid=ecf05e47c0o704ptclmfyfne1vs3umapzjre1ycvk5dtz2hc&ep=v1_gifs_search&rid=giphy.gif&ct=g)",
+		};
+		const defaultImage = "url(default-image-url.jpg)";
+		return backgroundImages[weather] || defaultImage;
+	};
+
 	return (
 		<div>
 			{forecastData ? (
@@ -56,7 +71,10 @@ const Forecast = () => {
 						{forecastData.list.map((item, index) => (
 							<Col key={index}>
 								<Card
+									id="weatherCard"
 									style={{
+										backgroundImage: getBackgroundImage(item.weather[0].main),
+										backgroundSize: "cover",
 										backgroundColor: "#87CEEB",
 										border: "none",
 										boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
