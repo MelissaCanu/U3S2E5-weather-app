610b6e66342bb3442b9931cbb9a5f59692928e4a
MelissaCanu <melissamcanu@gmail.com>
2024-01-12 11:58:15 +0100

    starting off, WeatherInfo component exported

---
+++ src/Components/WeatherInfo.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Button, Form, FormControl } from "react-bootstrap";
+
+const WeatherInfo = () => {
+	const [cityId, setCityId] = useState("");
+	const [cityInfo, setCityInfo] = useState(null);
+};
+
+export default WeatherInfo;
